bce5224b2b518e01f8a4321468c1b6fdabc1f951
rohith10e <rohith.e.s82@gmail.com>
2025-11-13 17:23:25 +0530

    feat: test-imap connection

---
+++ backend/src/imap/testImap.ts
@@ -0,0 +1,37 @@
+import { ImapFlow } from "imapflow";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function test() {
+  const client = new ImapFlow({
+    host: process.env.IMAP_HOST!,
+    port: Number(process.env.IMAP_PORT!),
+    secure: true,
+    auth: {
+      user: process.env.IMAP_USER_1!,
+      pass: process.env.IMAP_PASS_1!,
+    },
+  });
+
+  console.log("Connecting to IMAP...");
+
+  await client.connect();
+
+  console.log("Connected!");
+  console.log("Opening INBOX...");
+
+  await client.mailboxOpen("INBOX");
+
+  console.log("Fetching last 5 emails...");
+
+  const seq = (await client.search({ all: true }, { uid: true })) || [];
+  const last5 = seq.length ? seq.slice(-5) : [];
+
+  for await (let msg of client.fetch(last5, { envelope: true })) {
+    console.log("Email:", msg.envelope.subject);
+  }
+
+  await client.logout();
+}
+
+test();
